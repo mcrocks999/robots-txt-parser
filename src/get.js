824312e726deeb653ace86d31743f523b9671f10
Paul Makles <mcrocks999@users.noreply.github.com>
2018-02-03 20:30:23 +0000

    Use a pre-fetched robots.txt

--- src/get.js
+++ src/get.js
@@ -2,13 +2,7 @@ const get = require('simple-get');
 
 function getRobots(getUrl) {
   return new Promise((resolve, reject) => {
-    get.concat(getUrl, (error, response, body) => {
-      if (!error) {
-        resolve(body.toString());
-      } else {
-        reject(error);
-      }
-    });
+    resolve(getURL);
   });
 }
 
